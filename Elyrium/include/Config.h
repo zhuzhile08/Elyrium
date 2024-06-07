cf27be566fb4d4ee7817e6aec0bcd9f72193545c
Zhile Zhu <zhuzhile08@gmail.com>
2024-06-07 20:28:01 +0200

    More basic setup

---
+++ Elyrium/include/Config.h
@@ -0,0 +1,12 @@
+/*************************
+ * @file Config.h
+ * @author zhuzhile08 (zhuzhile08@gmail.com)
+ * 
+ * @brief Interpreter configs and settings
+ * 
+ * @date 2024-06-07
+ * 
+ * @copyright Copyright (c) 2024
+ *************************/
+
+#pragma once
--- Elyrium/src/main.cpp
+++ Elyrium/src/main.cpp
@@ -1,15 +1,30 @@
-#include <format>
-#include <print>
+#include <stdexcept>
+#include <chrono>
+
+#include <Common/Config.h>
+#include <Config.h>
+
+#include <fmt/core.h>
+#include <fmt/chrono.h>
 
 int main(int argc, char* argv[]) {
-	std::print("Hello");
-
 	if (argc > 1) {
-
-	} else if (argc == 1) {
+		
 
 	} else {
+		fmt::println(
+			"[{:%Ec %H:%M:%S}] | Elyrium Interpreter {} | Elyrium Base Library {}\nType \"todo\" for additional info.", 
+			fmt::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())), 
+			ELYRIUM_VERSION,
+			elyrium::config::version
+		);
 
+		fmt::print(">>>");
+
+		char buffer[2];
+		while(std::fgets(buffer, 2, stdin)) {
+			return 0;
+		}
 	}
 
 	return 0;
