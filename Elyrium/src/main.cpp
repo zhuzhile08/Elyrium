73b9686a3060055cf25c4036e737a02b4346a019
Zhile Zhu <zhuzhile08@gmail.com>
2024-06-08 13:16:57 +0200

    Add basic framework upon I can run the other stuff

--- Elyrium/src/main.cpp
+++ Elyrium/src/main.cpp
@@ -1,5 +1,7 @@
 #include <stdexcept>
+#include <cstdio>
 #include <chrono>
+#include <filesystem>
 
 #include <Common/Config.h>
 #include <Config.h>
@@ -7,10 +9,48 @@
 #include <fmt/core.h>
 #include <fmt/chrono.h>
 
+namespace {
+
+int runCmdEnv() {
+	fmt::print(">>> ");
+
+	/**
+	char buffer[2];
+	while(std::fgets(buffer, 2, stdin)) {
+		return 0;
+	}
+	*/
+
+	return 0;
+}
+
+int checkOptions(char* arg) {
+	fmt::println("Option given!");
+
+	return 0;
+}
+
+int runFile(char* path) {
+	auto globalPath = std::filesystem::current_path().append(path);
+	auto file = std::fopen(globalPath.c_str(), "r");
+
+	if (!file) {
+		fmt::println("elyrium: Could not open file at path \"{}\" with error: [Errno: {}] | {}", globalPath.c_str(), errno, std::strerror(errno));
+		
+		return errno;
+	} else {
+		fmt::println("File loaded!");
+
+		return 0;
+	}
+}
+
+}
+
 int main(int argc, char* argv[]) {
 	if (argc > 1) {
-		
-
+		if (*argv[1] == '-') return checkOptions(argv[1]);
+		else return runFile(argv[1]);
 	} else {
 		fmt::println(
 			"[{:%Ec %H:%M:%S}] | Elyrium Interpreter {} | Elyrium Base Library {}\nType \"todo\" for additional info.", 
@@ -18,14 +58,7 @@ int main(int argc, char* argv[]) {
 			ELYRIUM_VERSION,
 			elyrium::config::version
 		);
-
-		fmt::print(">>>");
-
-		char buffer[2];
-		while(std::fgets(buffer, 2, stdin)) {
-			return 0;
-		}
+		
+		return runCmdEnv();
 	}
-
-	return 0;
 }
