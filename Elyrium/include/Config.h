32b977f1130b9c4e0e380738f709bfc2f5b8a54e
Zhile Zhu <zhuzhile08@gmail.com>
2024-06-10 12:41:39 +0200

    Set input buffer starting size

--- Elyrium/include/Config.h
+++ Elyrium/include/Config.h
@@ -2,10 +2,16 @@
  * @file Config.h
  * @author zhuzhile08 (zhuzhile08@gmail.com)
  * 
- * @brief Interpreter configs and settings
+ * @brief Command line enviroment configs and settings
  * 
  * @date 2024-06-07
  * @copyright Copyright (c) 2024
  *************************/
 
 #pragma once
+
+namespace config {
+
+inline constexpr size_t inputBufferStartingSize = 256;
+
+}
